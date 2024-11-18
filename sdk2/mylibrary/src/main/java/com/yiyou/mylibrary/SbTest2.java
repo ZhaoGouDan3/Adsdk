db7f97b58810bfa91f5328ba016a3d72f6676a3c
ztl1310031723 <15364844617@163.com>
2024-11-18 15:33:30 +0800

    1.0.2

--- sdk2/mylibrary/src/main/java/com/yiyou/mylibrary/SbTest2.java
+++ sdk2/mylibrary/src/main/java/com/yiyou/mylibrary/SbTest2.java
@@ -3,8 +3,22 @@ package com.yiyou.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.anythink.core.api.ATAdConst;
+import com.anythink.core.api.ATSDK;
+import com.baidu.mobads.proxy.BuildConfig;
+
 public class SbTest2 {
     public static void test(Context context){
         Toast.makeText(context, "傻逼", Toast.LENGTH_SHORT).show();
+        if (BuildConfig.DEBUG) {
+            ATSDK.setNetworkLogDebug(true);
+            /**
+             * 注意：不要在提交上架审核的包中带上此API，否则可能影响上架
+             */
+            ATSDK.integrationChecking(context.getApplicationContext());
+        }
+        ATSDK.setPersonalizedAdStatus(ATAdConst.PRIVACY.PERSIONALIZED_ALLOW_STATUS);
+        ATSDK.init(context,"", "");
+        ATSDK.start();
     }
 }

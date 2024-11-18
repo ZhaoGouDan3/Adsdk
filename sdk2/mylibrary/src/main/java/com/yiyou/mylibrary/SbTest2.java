28519b7f4e914e153aabafe64f6d45e66a7ee2f2
ztl1310031723 <15364844617@163.com>
2024-11-18 14:10:43 +0800

    no message

---
+++ sdk2/mylibrary/src/main/java/com/yiyou/mylibrary/SbTest2.java
@@ -0,0 +1,10 @@
+package com.yiyou.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class SbTest2 {
+    public static void test(Context context){
+        Toast.makeText(context, "傻逼", Toast.LENGTH_SHORT).show();
+    }
+}

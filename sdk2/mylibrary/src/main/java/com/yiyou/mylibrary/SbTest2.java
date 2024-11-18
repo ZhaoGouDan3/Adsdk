5c295328e570629fc41c8faf757177d891c7debe
ztl1310031723 <15364844617@163.com>
2024-11-18 17:04:44 +0800

    no message

--- sdk2/mylibrary/src/main/java/com/yiyou/mylibrary/SbTest2.java
+++ sdk2/mylibrary/src/main/java/com/yiyou/mylibrary/SbTest2.java
@@ -9,15 +9,15 @@ import android.widget.Toast;
 public class SbTest2 {
     public static void test(Context context){
         Toast.makeText(context, "傻逼", Toast.LENGTH_SHORT).show();
-//        if (BuildConfig.DEBUG) {
-//            ATSDK.setNetworkLogDebug(true);
-//            /**
-//             * 注意：不要在提交上架审核的包中带上此API，否则可能影响上架
-//             */
-//            ATSDK.integrationChecking(context.getApplicationContext());
-//        }
-//        ATSDK.setPersonalizedAdStatus(ATAdConst.PRIVACY.PERSIONALIZED_ALLOW_STATUS);
-//        ATSDK.init(context,"", "");
-//        ATSDK.start();
+        if (BuildConfig.DEBUG) {
+            ATSDK.setNetworkLogDebug(true);
+            /**
+             * 注意：不要在提交上架审核的包中带上此API，否则可能影响上架
+             */
+            ATSDK.integrationChecking(context.getApplicationContext());
+        }
+        ATSDK.setPersonalizedAdStatus(ATAdConst.PRIVACY.PERSIONALIZED_ALLOW_STATUS);
+        ATSDK.init(context,"", "");
+        ATSDK.start();
     }
 }

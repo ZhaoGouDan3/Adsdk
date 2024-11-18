29edb11fc9d38a53240593c7aef22b6ac16764fe
ztl1310031723 <15364844617@163.com>
2024-11-18 11:43:46 +0800

    no message

---
+++ sdk2/app/src/main/java/com/yiyou/sdk/SbTest.java
@@ -0,0 +1,10 @@
+package com.yiyou.sdk;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class SbTest {
+    public static void test(Context context){
+        Toast.makeText(context, "傻逼", Toast.LENGTH_SHORT).show();
+    }
+}
